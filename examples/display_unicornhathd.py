0b7e8d6fb5ba88b862c0e11fb41e084b3d7c9f76
6871 <55576043+6871@users.noreply.github.com>
2020-03-23 23:04:14 +0000

    Add content function parameters

--- examples/display_unicornhathd.py
+++ examples/display_unicornhathd.py
@@ -5,7 +5,7 @@ from matrix_display.displays import UnicornHATHD
 import row_functions as f
 
 conveyor = Conveyor(UnicornHATHD(16, 16).rotation(180))
-conveyor.add_row(f.get_sine_wave_canvas, 1)  # >= 1 second refresh
+conveyor.add_row(f.get_sine_wave_canvas, 1, 16, 6)  # >= 1 second refresh
 conveyor.add_row(f.get_clock_text_tuple_list, 5)  # >= 5 second refresh
 conveyor.add_row(f.get_seconds_text_tuple, 0.25)  # >= 0.25 second refresh
 conveyor.play()
